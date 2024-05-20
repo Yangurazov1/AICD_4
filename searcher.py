fa09cdfc796a1254262562353f513e427857d8e3
Mansur Ziiatdinov <gltronred@gmail.com>
2024-05-20 09:45:14 +0300

    Show results count

--- searcher.py
+++ searcher.py
@@ -14,9 +14,17 @@ from sset import SSet
 FILENAME = "small-words.txt"
 
 # Messages for footer
-CTRL_C = Text.from_markup("Press [yellow]Ctrl-C[/] to exit.")
-ONE_MORE = Text.from_markup("Press [yellow]Ctrl-C[/] to exit. [bold]Type one more letter...")
 LOADING = Text.from_markup("Loading dictionary...")
+SHOW_LIMIT = 1000
+
+
+def footer_markup(cnt: int) -> Text:
+    """Returns markup for footer text."""
+    if cnt == 0:
+        return Text.from_markup("Press [yellow]Ctrl-C[/] to exit.")
+    if cnt <= SHOW_LIMIT:
+        return Text.from_markup(f"Press [yellow]Ctrl-C[/] to exit. [blue]{cnt}[/] results.")
+    return Text.from_markup(f"Press [yellow]Ctrl-C[/] to exit. [blue]{cnt}[/] results. [bold]Type one more letter...")
 
 
 class SearcherApp(App):
@@ -44,23 +52,18 @@ class SearcherApp(App):
         """Called when app is ready"""
         self.sset.load()
         self.query_one("#results", RichLog).loading = False
-        self.query_one(Label).update(CTRL_C)
+        self.query_one(Label).update(footer_markup(0))
 
     async def on_input_changed(self, message: Input.Changed) -> None:
         """Handles a changed text message."""
         log = self.query_one("#results", RichLog)
-        if len(message.value) > 2:
-            self.query_one(Label).update(CTRL_C)
+        if len(message.value) > 0:
+            self.query_one(Label).update(footer_markup(0))
             log.loading = True
             self.lookup_word(message.value)
-            log.loading = False
-        elif len(message.value) > 0:
-            # Don't search for single letter: too many results
-            log.loading = True
-            self.query_one(Label).update(ONE_MORE)
         else:
             # Clear results
-            self.query_one(Label).update(CTRL_C)
+            self.query_one(Label).update(footer_markup(0))
             log.loading = False
             log.clear()
 
@@ -68,20 +71,24 @@ class SearcherApp(App):
     async def lookup_word(self, substr: str) -> None:
         """Writes a list of words into results."""
         results = self.sset.search(substr)
-        log = self.query_one("#results", RichLog)
-        log.clear()
-        for w in results:
-            pos = w.find(substr)
-            if pos == -1:
-                # wrong, no substr in word
-                log.write(Text.from_markup(f"[strike bold red]{w}"))
-            else:
-                n = len(substr)
-                before = w[0:pos]
-                word = w[pos:pos+n]
-                after = w[pos+n:]
-                markup = before + "[bold green]" + word + "[/]" + after
-                log.write(Text.from_markup(markup))
+        cnt = len(results)
+        if cnt <= SHOW_LIMIT:
+            log = self.query_one("#results", RichLog)
+            log.clear()
+            for w in results:
+                pos = w.find(substr)
+                if pos == -1:
+                    # wrong, no substr in word
+                    log.write(Text.from_markup(f"[strike bold red]{w}"))
+                else:
+                    n = len(substr)
+                    before = w[0:pos]
+                    word = w[pos:pos+n]
+                    after = w[pos+n:]
+                    markup = before + "[bold green]" + word + "[/]" + after
+                    log.write(Text.from_markup(markup))
+            log.loading = False
+        self.query_one(Label).update(footer_markup(cnt))
 
 
 if __name__ == "__main__":
